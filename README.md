e1175dd647a83be449f246da2646cc2e8ec2399d
Seojun <brave605@naver.com>
2024-07-14 14:45:08 +0900

    Basic

---
+++ README.md
@@ -0,0 +1,16 @@
+# Wetube Reloaded
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/:id -> See User
+/users/logout -> Log out
+/users/edit -> Edit My profile
+/users/delete -> Delete My profile
+
+/videos/:id -> See Video
+/videos/:id/edit -> Edit Video
+/videos/:id/delete -> Delete Video 
+/videos/upload -> Upload Video
---
+++ src/controllers/videoController.js
@@ -0,0 +1,12 @@
+export const trending = (req, res) => res.render("home");
+
+export const see = (req, res) => res.render("watch");
+
+export const edit = (req, res) => res.render("edit"); 
+
+export const search = (req, res) => res.send("Search");
+export const upload = (req, res) => res.send("uploads");
+
+export const deleteVideo = (req, res) => {
+    return res.send(`Delete Videos ${req.params.id}`);
+}; 
