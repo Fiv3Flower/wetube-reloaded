4ca7b196f51bcf74c7b71cfd2f71b94842dbcb73
Seojun <brave605@naver.com>
2024-10-02 17:00:36 +0900

    restart!

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,42 @@
-export const trending = (req, res) => res.render("home");
+import Video from "../models/Video";
 
-export const see = (req, res) => res.render("watch");
+export const home = async(req, res) => {
+    const videos = await Video.find({})
+    return res.render("home", {pageTitle: "Home", videos});
+};
+export const watch = async(req, res) => {
+    const { id } = req.params;
+    const video = await Video.findById(id) //여기서 부터 다시 시작
+    return res.render("watch", {pageTitle: `Watching`, video});
+}
+export const getEdit = (req, res) => {
+    const { id } = req.params;
+    return res.render("edit", {pageTitle: `Editing`}); 
+}
+export const postEdit = (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+    return res.redirect(`/videos/${id}`);
+}
 
-export const edit = (req, res) => res.render("edit"); 
+// Test forms
+export const getUpload = (req, res) => {
+    return res.render("upload", {pageTitle: "Upload Video"});
+};
 
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("uploads");
-
-export const deleteVideo = (req, res) => {
-    return res.send(`Delete Videos ${req.params.id}`);
-}; 
+export const postUpload = async(req, res) => {
+    const { title, description, hashtags } = req.body;
+    try {
+    await Video.create({
+        title: title,
+        description: description,
+        hashtags: hashtags.split(",").map((word) => `#${word}`),
+    });
+    return res.redirect("/");
+    } catch(error) {
+        return res.render("upload", {
+            pageTitle: "Upload Video", 
+            errorMessage: error._message
+        });
+    }
+}
